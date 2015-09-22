2ba28fe08a7889cc9028fede55447ca7c7c2d3c3
Rene Donner <code@donner.at>
2015-09-22 09:29:14 +0200

    fix for 0.4rc2

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,16 @@
 println("\n\n\nStarting runtests.jl $(join(ARGS, " ")) ...")
-using FactCheck, KShiftsClustering, Compat
+using FactCheck, KShiftsClustering
 
 getdata(n) = 10*rand(1, n) + 0.5
 facts("all") do
     centers = kshifts(getdata(1_000_000), 10)
-    @fact round(Int, sort(vec(centers)))  =>  [1:10]
+    @fact round(Int, sort(vec(centers)))  -->  collect(1:10)
 
     centers = kshifts(getdata(1000), 10)
     for i = 1:100
         kshifts!(centers, getdata(1000))
     end
-    @fact round(Int, sort(vec(centers)))  =>  [1:10]
+    @fact round(Int, sort(vec(centers)))  -->  collect(1:10)
 
     centers, ids = kshiftmedoids(rand(10, 100000), 10)
 
