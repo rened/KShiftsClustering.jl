7dddecc1053675b35e27170c456aa21ebe81ff7d
Viktor Petukhov <viktor.s.petuhov@ya.ru>
2019-04-24 17:04:30 -0700

    Removed dependencies. Julia 1.0 compatibility.

--- src/KShiftsClustering.jl
+++ src/KShiftsClustering.jl
@@ -1,65 +1,61 @@
-__precompile__()
-
 module KShiftsClustering
 
-using FunctionalData, FunctionalDataUtils, NearestNeighbors
+using NearestNeighbors
 
 export kshifts, kshifts!, kshiftslabels, kshiftslabels!, kshiftmedoids
 
-function kshiftmedoids(data,k)
+function kshiftmedoids(data, k)
     centers = kshifts(data,k)
     tree = KDTree(float(data))
-    knnID(a) = @p knn tree vec(a) 1  | fst | fst
-    ids = @p map centers knnID
-    part(data, ids), ids
+    ids = vec(mapslices(a -> knn(tree, a, 1)[1][1], centers, dims=1))
+
+    return data[:,ids], ids
 end
 
-
-@inbounds @inline function dist{T}(a::Matrix{T}, i::Int, b::Matrix{T}, j::Int)
-    sum = zeroel(a)
+@inbounds @inline function dist(a::Matrix{T}, i::Int, b::Matrix{T}, j::Int) where T
+    sum = zero(eltype(a))
     @simd for d = 1:size(a,1)
         sum += (a[d,i]-b[d,j])*(a[d,i]-b[d,j])
     end
-    sum
+
+    return sum
 end
 
-function kshifts{T}(data::Array{T,2}, k::Int)
-    centers = @p randsample data k
+function kshifts(data::Array{T,2} where T, k::Int)
+    centers = data[:,rand(1:size(data, 2), k)]
     kshifts!(centers, data)
 end
 
-@inbounds function kshifts!{T}(centers::Array{T,2}, data::Array{T,2})
-    assert(sizem(data)==sizem(centers))
+@inbounds function kshifts!(centers::Array{T,2}, data::Array{T,2}) where T
+    @assert size(data, 1) == size(centers, 1)
     f1 = 29/30
     f2 = 1-f1
-    dv = FD.view(data)::Array{T,2}
-    cv = FD.view(centers)::Array{T,2}
-    for i = 1:len(data)
+    for i = 1:size(data, 2)
+        data_view = view(data, i)
         v = typemax(eltype(centers))
         ind = 0
-        for j = 1:len(centers)
+        for j = 1:size(centers, 2)
             d = dist(data, i, centers, j)
             if d < v
                 v = d
                 ind = j
             end
         end
-        view!(centers, ind, cv)
-        for d = 1:sizem(cv)
-            cv[d] = f1*cv[d] + f2*dv[d]
+        center_view = view(centers, ind)
+        for d = 1:size(center_view, 1)
+            center_view[d] = f1*center_view[d] + f2*data_view[d]
         end
-        next!(dv)
     end
     centers
 end
 
-kshiftslabels(data, centers) = kshiftslabels!(zeros(Int,len(data)),data, centers)
-@inbounds function kshiftslabels!{T}(labels::Vector{Int}, data::Matrix{T}, centers::Matrix{T})
+kshiftslabels(data, centers) = kshiftslabels!(zeros(Int, size(data, 2)), data, centers)
+@inbounds function kshiftslabels!(labels::Vector{Int}, data::Matrix{T}, centers::Matrix{T}) where T
     # dv = FD.view(data)::Matrix{T}
-    for i = 1:len(data)
+    for i = 1:size(data, 2)
         v = typemax(T)
         ind = 0
-        for j = 1:len(centers)
+        for j = 1:size(centers, 2)
             d = dist(data, i, centers, j)
             if d < v
                 v = d
